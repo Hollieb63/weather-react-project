61cc99b5fd3e906c5db51582e10cf514f4c156e0
Hollieb63 <127986870+Hollieb63@users.noreply.github.com>
2024-03-13 17:48:43 +0000

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-   Weather App
+   <h1>Weather App</h1>
+   <footer>
+    This project was coded by Hollie Bradshaw, and is open-sourced on 
+    <a href="https://github.com/Hollieb63/weather-react-project.git"> GitHub</a>
+   </footer>
     </div>
   );
 }
