05ecd669c67eb2ce7e9087d8a83d664ec1e12678
Hollieb63 <127986870+Hollieb63@users.noreply.github.com>
2024-03-13 18:53:09 +0000

    Added weather js/css file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather.js";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
    <h1>Weather App</h1>
+   <Weather/>
    <footer>
     This project was coded by Hollie Bradshaw, and is open-sourced on 
     <a href="https://github.com/Hollieb63/weather-react-project.git"> GitHub</a>
    </footer>
+  
    </div>
     </div>
   );
