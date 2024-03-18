8e5c340528a2d71a1d42b09335b2a1aec223fcb1
Hollieb63 <127986870+Hollieb63@users.noreply.github.com>
2024-03-18 22:16:07 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     
-    <App rel="noopener noreferrer"/>
+    <App/>
   </React.StrictMode>
 );
 
